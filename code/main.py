e9fbdfadc3bb9d243966200d009489bf07a65e34
bryanrabbit <34449029+bryanrabbit@users.noreply.github.com>
2023-01-16 21:32:18 -0500

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,77 +1,149 @@
-import pygame, sys, random
+"""
+GOALS:
+
+- add original music
+- change laser sounds (player laser and alien lasers should be different)(make alien laser sound less annoying or constant)
+- get scrolling screen to work (was not second blitting, now won't move at all?)
+- add unique start and game overscreens, game_active state
+- add scoring system
+- collision without groupcollide?
+- add music channels
+- allow user to change volume in game (options menu?)
+- add animations
+"""
+
+import pygame, sys, random, time
+from settings import *
 from player import Player
 from alien import Alien
 from laser import Laser
+from background import BG
 
 class Game:
 	def __init__(self):
+
+		# Game setup
+		pygame.init()
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), pygame.SCALED)
+		pygame.display.set_caption('Star Fighter')
+		self.clock = pygame.time.Clock()
+
+		# Timers
+		self.alien_timer = pygame.USEREVENT + 1
+		pygame.time.set_timer(self.alien_timer,2000) # lower numbers = faster rate of enemy spawn
+
+		self.ALIENLASER = pygame.USEREVENT + 1
+		pygame.time.set_timer(self.ALIENLASER,400)
+
+		# Background Setup
+		self.background = pygame.sprite.Group()
+
+		# scale factor
+		bg_width = pygame.image.load('../graphics/background.png').get_width()
+		self.scale_factor = SCREEN_WIDTH / bg_width
+
+		BG(self.background,self.scale_factor)
+
 		# Player setup
-		player_sprite = Player((screen_width/2,screen_height/2),screen_width,screen_height,5)
+		player_sprite = Player((SCREEN_WIDTH/2,SCREEN_HEIGHT/2),SCREEN_WIDTH,SCREEN_HEIGHT,5)
 		self.player = pygame.sprite.GroupSingle(player_sprite)
 
+		# Score setup
+		self.score = 0
+		self.font = pygame.font.Font('../font/Pixeled.ttf',20)
+
 		# Alien setup
-		#self.aliens = pygame.sprite.Group()
+		self.aliens = pygame.sprite.Group()
+		self.alien_lasers = pygame.sprite.Group()
 
 		# Audio
 		music = pygame.mixer.Sound('../audio/corneria.mp3')
-		music.set_volume(0.2)
+		music.set_volume(0.3)
 		music.play(loops = -1)
 		self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
-		self.laser_sound.set_volume(0.5)
+		self.laser_sound.set_volume(0.3)
 		self.explosion_sound = pygame.mixer.Sound('../audio/explosion.wav')
-		self.explosion_sound.set_volume(0.3)
+		self.explosion_sound.set_volume(0.5)
 
-	# def alien_setup():
-	# 	for event in events:
-	# 		if event.type == alien_timer:
-	# 				alien_color = random.choice(['red','green','yellow'])
-	# 				aliens.add(Alien(alien_color,screen_width,screen_height))
+	def spawn_aliens(self,alien_color):
+		self.aliens.add(Alien(alien_color,SCREEN_WIDTH,SCREEN_HEIGHT))
 
-#	def collision_checks(self):
+	def alien_shoot(self):
+		if self.aliens.sprites():
+			random_alien = random.choice(self.aliens.sprites())
+			laser_sprite = Laser(random_alien.rect.center,6,SCREEN_HEIGHT)
+			self.alien_lasers.add(laser_sprite)
+			#self.laser_sound.play()
+
+	def collision_checks(self):
+		if self.player.sprite.lasers:
+			for laser in self.player.sprite.lasers:
+				# why does groupcollide work but in the Space Invaders code spritecollide works with the alien group?
+				# can't use score like space invaders code because group object has no attribute value?
+				if pygame.sprite.groupcollide(self.player.sprite.lasers,self.aliens,False,True):
+					laser.kill()
+					self.explosion_sound.play()
+					self.score += 1
+		if self.alien_lasers:
+			for laser in self.alien_lasers:
+				if pygame.sprite.spritecollide(laser,self.player,False): # change to game over
+					laser.kill()
+					pygame.quit()
+					sys.exit()
+		if pygame.sprite.spritecollide(self.player.sprite,self.aliens,False): # game over if you touch a ship
+			pygame.quit()
+			sys.exit()
+
+	# def score_drop(self):
+	# 	for alien in self.aliens.sprites():
+	# 		if alien.rect.bottom == SCREEN_HEIGHT:
+	# 			self.score -= 1 # doesn't work every single time
+			
+	def display_score(self):
+		score_surf = self.font.render(f'score: {self.score}',False,'white')
+		score_rect = score_surf.get_rect(topleft = (10,-10))
+		self.screen.blit(score_surf,score_rect)
 
 	def run(self):
-		self.player.update()
+		last_time = time.time()
+		while True:
+			# delta time
+			dt = time.time() - last_time
+			last_time = time.time()
 
-		#self.aliens.update()
-		#self.collision_checks()
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+				if event.type == self.alien_timer:
+					alien_color = random.choice(['red','green','yellow'])
+					game.spawn_aliens(alien_color)
+				if event.type == self.ALIENLASER:
+					game.alien_shoot()
+				if event.type == pygame.KEYDOWN:
+					if event.mod & pygame.KMOD_ALT and event.key in [pygame.K_RETURN, pygame.K_KP_ENTER]:
+						pygame.display.toggle_fullscreen()
 
-		self.player.sprite.lasers.draw(screen)
-		self.player.draw(screen)
-		#self.aliens.draw(screen)
+			self.screen.fill((30,30,30))
+			self.background.update(dt)
+			self.background.draw(self.screen)
+
+			self.player.update()
+			self.alien_lasers.update()
+
+			self.aliens.update()
+			self.collision_checks()
+
+			self.player.sprite.lasers.draw(self.screen)
+			self.player.draw(self.screen)
+			self.aliens.draw(self.screen)
+			self.alien_lasers.draw(self.screen)
+			# self.score_drop()
+			self.display_score()
+
+			pygame.display.flip()
+			self.clock.tick(FRAMERATE)
 
 if __name__ == '__main__':
-	pygame.init()
-	screen_width = 600
-	screen_height = 800
-	screen = pygame.display.set_mode((screen_width,screen_height))
-	clock = pygame.time.Clock()
 	game = Game()
-
-	# Alien setup (ry to put this in the Game class)
-	aliens = pygame.sprite.Group()
-
-	# Timer 
-	alien_timer = pygame.USEREVENT + 1
-	pygame.time.set_timer(alien_timer,200) # lower numbers = faster rate of enemy spawn
-
-
-	# Multiple event loops? Or same event loop in class and main loop?
-	#events = pygame.event.get():
-
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
-			if event.type == alien_timer:
-				alien_color = random.choice(['red','green','yellow'])
-				aliens.add(Alien(alien_color,screen_width,screen_height))
-
-		screen.fill((30,30,30))
-		game.run()
-
-		aliens.draw(screen)
-		aliens.update()
-			
-		pygame.display.flip()
-		clock.tick(60)
+	game.run()
