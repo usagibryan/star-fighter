5bf7e752ef0301a5b3c997fa7c4084dccfb85772
bryanrabbit <34449029+bryanrabbit@users.noreply.github.com>
2023-01-18 22:17:03 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -5,10 +5,13 @@ GOALS:
 - add original music
 - add original sprites
 - add original sound effects
+- add explosions for enemies
+- add explosion/death animation for player, delay game over screen?
 - change laser sounds (player laser and alien lasers should be different)(make alien laser sound less annoying or constant)
 - add menu
 - add pause option
 - add lives or health bar
+- add multiple levels/stages
 - add music channels
 - allow user to change volume in game (options menu?)
 - add animations
