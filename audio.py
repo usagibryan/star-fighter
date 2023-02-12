04b34dd88747fa65b61a178ce9980c78bd3069c7
usagibryan <bryanrabbit@gmail.com>
2023-02-11 20:32:49 -0500

    added master volume

--- audio.py
+++ audio.py
@@ -4,41 +4,43 @@ from settings import *
 class Audio():
     def __init__(self):
         super().__init__()
+        self.master_volume = 1.0 # default value is 1.0
+
         self.intro_music = pygame.mixer.Sound('audio/star_fox_controls.mp3')
-        self.intro_music.set_volume(.5)
+        self.intro_music.set_volume(.5 * self.master_volume)
         self.channel_0 = pygame.mixer.Channel(0)
         self.play_intro_music = True # Set to False after user begins, only place once
 
         self.bg_music = pygame.mixer.Sound('audio/star_hero.mp3')
-        self.bg_music.set_volume(1) # very low for some reason
+        self.bg_music.set_volume(1 * self.master_volume) # very low for some reason
         self.channel_1 = pygame.mixer.Channel(1)
 
         self.explosion_sound = pygame.mixer.Sound('audio/explosion.wav')
-        self.explosion_sound.set_volume(0.3)
+        self.explosion_sound.set_volume(0.3 * self.master_volume)
         self.channel_2 = pygame.mixer.Channel(2)
 
         self.laser_sound = pygame.mixer.Sound('audio/laser.wav')
-        self.laser_sound.set_volume(0.3)
+        self.laser_sound.set_volume(0.3 * self.master_volume)
         self.channel_3 = pygame.mixer.Channel(3)
 
         # Low Health Alarms share channel
         self.low_health_alarm1 = pygame.mixer.Sound('audio/sfx_alarm_loop2.wav')
-        self.low_health_alarm1.set_volume(0.3)
+        self.low_health_alarm1.set_volume(0.3 * self.master_volume)
         self.low_health_alarm2 = pygame.mixer.Sound('audio/sfx_alarm_loop1.wav')
-        self.low_health_alarm2.set_volume(0.3)
+        self.low_health_alarm2.set_volume(0.3 * self.master_volume)
         self.channel_4 = pygame.mixer.Channel(4)
 
         self.ufo_sound = pygame.mixer.Sound('audio/sfx_sound_bling.wav')
-        self.ufo_sound.set_volume(1)
+        self.ufo_sound.set_volume(1 * self.master_volume)
         self.channel_5 = pygame.mixer.Channel(5)
 
         self.pause_sound = pygame.mixer.Sound('audio/sfx_sounds_pause2_in.wav')
-        self.pause_sound.set_volume(1)
+        self.pause_sound.set_volume(1 * self.master_volume)
         self.channel_6 = pygame.mixer.Channel(6)
 
         self.unpause_sound = pygame.mixer.Sound('audio/sfx_sounds_pause2_out.wav')
-        self.unpause_sound.set_volume(1)
+        self.unpause_sound.set_volume(1 * self.master_volume)
         self.channel_7 = pygame.mixer.Channel(7)
 
         self.player_down = pygame.mixer.Sound('audio/player_down.mp3')
-        self.player_down.set_volume(.5)
+        self.player_down.set_volume(.5 * self.master_volume)
