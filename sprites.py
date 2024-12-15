3982b3ba728f6d3f4421af98fed1d858ca8284cb
Frankie <34449029+usagibryan@users.noreply.github.com>
2024-12-15 11:35:42 -0500

    Update controller input

--- sprites.py
+++ sprites.py
@@ -71,22 +71,24 @@ class Player(pygame.sprite.Sprite):
             self.laser_time = pygame.time.get_ticks()
             self.audio.channel_3.play(self.audio.laser_sound)
 
-        # # USB controller input (doesn't work properly)
-        # pygame.joystick.init()
-        # joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+        # # USB controller input
+        pygame.joystick.init()
+        joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 
-        # # Xbox USB controller input
-        # x_speed = round(pygame.joystick.Joystick(0).get_axis(0))
-        # y_speed = round(pygame.joystick.Joystick(0).get_axis(1))
-        # self.joystick_move(x_speed,y_speed)
+        # USB controller input
+        # movement is VERY SLOW for some reason
+        x_speed = round(pygame.joystick.Joystick(0).get_axis(0))
+        y_speed = round(pygame.joystick.Joystick(0).get_axis(1))
+        self.joystick_move(x_speed,y_speed)
 
-        # for event in pygame.event.get():
-        #   if event.type == pygame.JOYBUTTONDOWN:
-        #       if pygame.joystick.Joystick(0).get_button(0) and self.ready: # A button shoots the laser
-        #           self.shoot_laser()
-        #           self.ready = False
-        #           self.laser_time = pygame.time.get_ticks()
-        #           self.audio.channel_3.play(self.audio.laser_sound)
+        # Laser is poorly responsive
+        for event in pygame.event.get():
+          if event.type == pygame.JOYBUTTONDOWN:
+              if pygame.joystick.Joystick(0).get_button(0) and self.ready: # A button shoots the laser
+                  self.shoot_laser()
+                  self.ready = False
+                  self.laser_time = pygame.time.get_ticks()
+                  self.audio.channel_3.play(self.audio.laser_sound)
 
     def recharge(self):
         if not self.ready:
